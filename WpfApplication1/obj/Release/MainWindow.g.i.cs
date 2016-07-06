454f40b4f3b9477ee4834bf61254d731594c55ca
zyang62GT <zyang62@gmail.com>
2016-07-06 11:29:04 -0400

    changed project name

--- WpfApplication1/obj/Release/MainWindow.g.i.cs
+++ WpfApplication1/obj/Release/MainWindow.g.i.cs
@@ -76,7 +76,7 @@ namespace WpfApplication1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApplication1;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SocialHighlighter;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
